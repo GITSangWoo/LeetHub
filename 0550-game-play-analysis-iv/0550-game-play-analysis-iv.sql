f2fff26b04899068861e3e9df313d236ebca7843
이상우 <dogcenta@gmail.com>
2025-04-04 00:03:47 +0900

    Time: 511 ms (98.87%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,39 +1,12 @@
--- select *
--- from Activity
--- group by player_id,device_id
--- order by player_id,event_date
-
--- select *, FIRST_VALUE(event_date)OVER(partition by player_id, device_id order by player_id) as first_logged_date 
--- ,LEAD(event_date,1) OVER(partition by player_id,device_id order by player_id,event_date) as second_played_date
--- from Activity
-
--- select *, FIRST_VALUE(event_date)OVER(partition by player_id order by event_date) as first_logged_date 
--- ,LEAD(event_date,1) OVER(partition by player_id order by player_id,event_date) as second_played_date
--- from Activity
-
--- select *, FIRST_VALUE(event_date)OVER(partition by player_id,device_id order by event_date) as first_logged_date 
--- ,LEAD(event_date,1) OVER(partition by player_id,device_id order by player_id,event_date) as second_played_date
--- from Activity
-
--- select sub.player_id,sub.device_id, case when sub.first_logged_date - sub.second_played_date = -1 then 1 END as special_user 
--- from 
--- (select *, FIRST_VALUE(event_date)OVER(partition by player_id, device_id order by player_id) as first_logged_date 
--- ,LEAD(event_date,1) OVER(partition by player_id,device_id order by player_id,event_date) as second_played_date
--- from Activity) as sub
--- where sub.first_logged_date - sub.second_played_date = - 1
-
-
-
--- select ROUND(count(case when sub.first_logged_date - sub.second_played_date = -1 then 1 END)/count(DISTINCT sub.player_id) as fraction
--- from 
--- (select *, FIRST_VALUE(event_date)OVER(partition by player_id,device_id order by event_date) as first_logged_date 
--- ,LEAD(event_date,1) OVER(partition by player_id,device_id order by player_id,event_date) as second_played_date
--- from Activity) as sub
-
-
-select round(count(case when DATEDIFF(sub.first_logged_date,sub.second_played_date) = -1 then 1 END)/count(DISTINCT sub.player_id),2) as fraction
-from 
-(select *, FIRST_VALUE(event_date)OVER(partition by player_id order by event_date) as first_logged_date 
-,LEAD(event_date,1) OVER(partition by player_id order by player_id,event_date) as second_played_date
-from Activity) as sub
+SELECT ROUND(
+    COUNT(CASE WHEN DATEDIFF(sub.second_played_date, sub.first_logged_date) = 1 THEN 1 END) /
+    COUNT(DISTINCT sub.player_id), 
+    2
+) AS fraction
+FROM (
+    SELECT *, 
+           FIRST_VALUE(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS first_logged_date,
+           LEAD(event_date, 1) OVER (PARTITION BY player_id ORDER BY event_date) AS second_played_date
+    FROM Activity
+) AS sub;
 
