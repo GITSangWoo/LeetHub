8ea97eedfd9d246e1a1d92e71d9bd3f2e6aa7fea
이상우 <dogcenta@gmail.com>
2025-08-05 18:08:57 +0900

    Time: 1769 ms (20.23%), Space: 0B (100%) - LeetHub

--- 1907-count-salary-categories/1907-count-salary-categories.sql
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,14 +1,31 @@
-SELECT category,
-       COUNT(a.account_id) AS accounts_count
-FROM (
-    SELECT 'Low Salary' AS category
-    UNION ALL
-    SELECT 'Average Salary'
-    UNION ALL
-    SELECT 'High Salary'
-) AS cats
-LEFT JOIN Accounts a
-  ON (cats.category = 'Low Salary' AND a.income < 20000)
-  OR (cats.category = 'Average Salary' AND a.income BETWEEN 20000 AND 50000)
-  OR (cats.category = 'High Salary' AND a.income > 50000)
-GROUP BY category;
+-- SELECT category,
+--        COUNT(a.account_id) AS accounts_count
+-- FROM (
+--     SELECT 'Low Salary' AS category
+--     UNION ALL
+--     SELECT 'Average Salary'
+--     UNION ALL
+--     SELECT 'High Salary'
+-- ) AS cats
+-- LEFT JOIN Accounts a
+--   ON (cats.category = 'Low Salary' AND a.income < 20000)
+--   OR (cats.category = 'Average Salary' AND a.income BETWEEN 20000 AND 50000)
+--   OR (cats.category = 'High Salary' AND a.income > 50000)
+-- GROUP BY category;
+
+
+SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+
+UNION ALL
+
+SELECT 'Average Salary', COUNT(*)
+FROM Accounts
+WHERE income BETWEEN 20000 AND 50000
+
+UNION ALL
+
+SELECT 'High Salary', COUNT(*)
+FROM Accounts
+WHERE income > 50000;
