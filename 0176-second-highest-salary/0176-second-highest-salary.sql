95d9c878751481da3783dc58b736aff8d5514cb8
이상우 <dogcenta@gmail.com>
2025-08-28 18:17:44 +0900

    Time: 1168 ms (5%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,14 +1,25 @@
 # Write your MySQL query statement below
 
-SELECT 
-    (
-        SELECT salary
-        FROM (
-            SELECT 
-                distinct salary,
-                DENSE_RANK() OVER (ORDER BY salary DESC) AS `rank`
-            FROM Employee
-        ) AS sub
-        WHERE `rank` = 2
-    ) AS SecondHighestSalary;
+-- SELECT distinct
+--     (
+--         SELECT  salary 
+--         FROM (
+--             SELECT 
+--                 salary,
+--                 dense_rank() OVER (ORDER BY salary DESC) AS `rank`
+--             FROM Employee
+--         ) AS sub
+--         WHERE `rank` = 2
+--     ) AS SecondHighestSalary;
+
+SELECT
+  (
+    SELECT salary
+    FROM (
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+    ) AS distinct_salaries
+    LIMIT 1 OFFSET 1  -- 두 번째로 큰 값
+  ) AS SecondHighestSalary;
 
